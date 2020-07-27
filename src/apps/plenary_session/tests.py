2e5407aac55e855fe86ab55ae9cb58f906370a3d
Thiago Nogueira <thiagonf10@gmail.com>
2020-07-27 17:47:03 -0300

    Adding tests

--- src/apps/plenary_session/tests.py
+++ src/apps/plenary_session/tests.py
@@ -1,78 +1,82 @@
-from django.core.exceptions import ValidationError
-from django.db.utils import IntegrityError
-
-# Create your tests here.
 import pytest
+from mixer.backend.django import mixer
 
 from .models import PlenarySession
-from datetime import datetime
+
+
+@pytest.mark.django_db
+def test_apps():
+    from apps.plenary_session.apps import PlenarySessionConfig
+    assert PlenarySessionConfig.name == 'plenary_session'
 
 
 @pytest.mark.django_db
 def test_plenary_session_create():
-    PlenarySession.objects.create(location='PLENARIO',
-                                  date=datetime.now(),
-                                  type_session='VIRTUAL',
-                                  situation_session='PRE_SESSAO',
-                                  resume="AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+    mixer.blend(PlenarySession)
     assert PlenarySession.objects.count() == 1
 
 
 @pytest.mark.django_db
 def test_plenary_session_create_erro_location():
-    with pytest.raises(ValidationError):
-        plenary_session = PlenarySession.objects.create(location='ERROR',
-                                                        date=datetime.now(),
-                                                        type_session='VIRTUAL',
-                                                        situation_session='PRE_SESSAO',
-                                                        resume="AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
-
+    with pytest.raises(Exception):
+        plenary_session = mixer.blend(PlenarySession, location='ERROR')
         plenary_session.full_clean()
 
 
 @pytest.mark.django_db
 def test_plenary_session_create_erro_date():
-    with pytest.raises(ValidationError):
-        plenary_session = PlenarySession.objects.create(location='PLENARIO',
-                                                        date='',
-                                                        type_session='VIRTUAL',
-                                                        situation_session='PRE_SESSAO',
-                                                        resume="AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
-
-        plenary_session.full_clean()
+    with pytest.raises(Exception):
+        mixer.blend(PlenarySession, date='')
 
 
 @pytest.mark.django_db
 def test_plenary_session_create_erro_type_session():
-    with pytest.raises(ValidationError):
-        plenary_session = PlenarySession.objects.create(location='PLENARIO',
-                                                        date=datetime.now(),
-                                                        type_session='ERROR',
-                                                        situation_session='PRE_SESSAO',
-                                                        resume="AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
-
+    with pytest.raises(Exception):
+        plenary_session = mixer.blend(PlenarySession, type_session='ERROR')
         plenary_session.full_clean()
 
 
 @pytest.mark.django_db
 def test_plenary_session_create_erro_situation_session():
-    with pytest.raises(ValidationError):
-        plenary_session = PlenarySession.objects.create(location='PLENARIO',
-                                                        date=datetime.now(),
-                                                        type_session='VIRTUAL',
-                                                        situation_session='ERROR',
-                                                        resume="AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
-
+    with pytest.raises(Exception):
+        plenary_session = mixer.blend(PlenarySession,
+                                      situation_session='ERROR')
         plenary_session.full_clean()
 
 
 @pytest.mark.django_db
 def test_plenary_session_create_erro_resume():
-    with pytest.raises(ValidationError):
-        plenary_session = PlenarySession.objects.create(location='PLENARIO',
-                                                        date=datetime.now(),
-                                                        type_session='VIRTUAL',
-                                                        situation_session='PRE_SESSAO',
-                                                        )
-
+    with pytest.raises(Exception):
+        plenary_session = mixer.blend(PlenarySession, resume='')
         plenary_session.full_clean()
+
+
+@pytest.mark.django_db
+def test_plenary_session_create_none_location():
+    with pytest.raises(Exception):
+        mixer.blend(PlenarySession, location=None)
+
+
+@pytest.mark.django_db
+def test_plenary_session_create_none_date():
+    with pytest.raises(Exception):
+        mixer.blend(PlenarySession, date=None)
+
+
+@pytest.mark.django_db
+def test_plenary_session_create_none_type_session():
+    with pytest.raises(Exception):
+        mixer.blend(PlenarySession, type_session=None)
+
+
+@pytest.mark.django_db
+def test_plenary_session_create_none_situation_session():
+    with pytest.raises(Exception):
+        mixer.blend(PlenarySession,
+                    situation_session=None)
+
+
+@pytest.mark.django_db
+def test_plenary_session_create_none_resume():
+    with pytest.raises(Exception):
+        mixer.blend(PlenarySession, resume=None)
