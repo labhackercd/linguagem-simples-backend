d39098710476e2186f88c4411f682e309302da70
erivanio <erivanio.vanin@gmail.com>
2020-07-29 11:50:14 -0300

    create admin with admin_generator

--- src/apps/authentication/admin.py
+++ src/apps/authentication/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
+
 from .models import CustomUser
 
 
+@admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
-    model = CustomUser
-
-
-admin.site.register(CustomUser, CustomUserAdmin)
+    list_display = (
+        'id',
+        'username',
+        'first_name',
+        'last_name',
+        'email',
+        'last_login',
+        'is_superuser',
+    )
+    list_filter = (
+        'last_login',
+        'is_superuser',
+        'is_staff',
+        'is_active',
+        'date_joined',
+    )
+    raw_id_fields = ('groups', 'user_permissions')
