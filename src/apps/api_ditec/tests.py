f447199416ba80eb92fc829387b74278f85a2aad
Thiago Nogueira <thiagonf10@gmail.com>
2020-08-05 09:28:12 -0300

    Adding test from api_ditec

--- src/apps/api_ditec/tests.py
+++ src/apps/api_ditec/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
-# Create your tests here.
+
+import pytest
+from unittest import mock
+
+from .views import get_news, get_filter_news
+
+
+@pytest.mark.django_db
+@mock.patch('apps.api_ditec.views.requests.request')
+def test_get_news(mock_request):
+    mock_request.return_value.ok = True
+
+    response = get_news()
+    assert response.data['id']
+    assert response
+
+
+@pytest.mark.django_db
+@mock.patch('apps.api_ditec.views.requests.request')
+def test_get_filter_news(mock_request):
+    mock_request.return_value.ok = True
+
+    response = get_filter_news('words')
+    assert response.data['id']
+    assert response
