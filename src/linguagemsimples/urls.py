ce6c0edd35ace68948c9e9d6cf13ca7e52c810e7
teogenesmoura <mourateogenes@gmail.com>
2020-08-06 17:06:56 -0300

    #11 Adding base html template and styling for password recovery pages

--- src/linguagemsimples/urls.py
+++ src/linguagemsimples/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from django.views.generic import TemplateView 
+from django.views.generic import TemplateView
 
 schema_view = get_schema_view(
     openapi.Info(
