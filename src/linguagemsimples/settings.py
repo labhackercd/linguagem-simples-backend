69716c95d334b36bcd0b3335dcaa788101635559
erivanio <erivanio.vanin@gmail.com>
2020-08-06 11:50:45 -0300

    create folders to templates and styles

--- src/linguagemsimples/settings.py
+++ src/linguagemsimples/settings.py
@@ -71,7 +71,9 @@ ROOT_URLCONF = 'linguagemsimples.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
