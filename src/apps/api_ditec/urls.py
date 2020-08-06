460145e286dca9f8b3adbdc632a40e85e29c9e68
Thiago Nogueira <thiagonf10@gmail.com>
2020-08-06 11:11:29 -0300

    #21 - Adding urls from tvcamara

--- src/apps/api_ditec/urls.py
+++ src/apps/api_ditec/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import ListNews, SearchNews, ListRadioagency, SearchRadioagency
+from .views import (ListNews, SearchNews, ListRadioagency, SearchRadioagency,
+                    ListTvCamara, SearchTvCamara)
 
 urlpatterns = [
     path('news/', ListNews.as_view(), name='news'),
@@ -7,4 +8,7 @@ urlpatterns = [
     path('radioagency/', ListRadioagency.as_view(), name='radioagency'),
     path('radioagency-search/', SearchRadioagency.as_view(),
          name='radioagency-search'),
+    path('tvcamara/', ListTvCamara.as_view(), name='tvcamara'),
+    path('tvcamara-search/', SearchTvCamara.as_view(),
+         name='tvcamara-search'),
 ]
