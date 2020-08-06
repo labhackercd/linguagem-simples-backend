e40a08c8c5353d51ce5279709d26bae1febebf6c
Thiago Nogueira <thiagonf10@gmail.com>
2020-08-06 09:12:50 -0300

    Us08 atualizações radio câmara (#39)
    * Refactoring method to get subject and search subject
    
    * Adding views list and search from radioagency in api_ditec
    
    * Adding url and tests radioagency in api_ditec

--- src/apps/api_ditec/urls.py
+++ src/apps/api_ditec/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import ListNews, SearchNews
+from .views import ListNews, SearchNews, ListRadioagency, SearchRadioagency
 
 urlpatterns = [
     path('news/', ListNews.as_view(), name='news'),
     path('news-search/', SearchNews.as_view(), name='news-search'),
+    path('radioagency/', ListRadioagency.as_view(), name='radioagency'),
+    path('radioagency-search/', SearchRadioagency.as_view(),
+         name='radioagency-search'),
 ]
--- src/apps/api_ditec/views.py
+++ src/apps/api_ditec/views.py
@@ -8,49 +8,71 @@ from django.utils.translation import gettext_lazy as _
 
 
 class ListNews(APIView):
+    path = '/noticias/_search'
 
     def get(self, request):
-        news = self.get_news()
-        return Response(news)
-
-    def get_news(self):
-        path = '/noticias/_search'
-        query = DEFAULT_QUERY.replace('replace_query', DATE_QUERY)
-        query = query.replace('NW', NUMBER_WEEKS)
-
-        response = requests.request('POST', settings.API_DITEC + path,
-                                    headers=HEADERS,
-                                    data=query)
-        try:
-            response = response.json()
-        except Exception:
-            response = {'error': _('Error not found news')}
-
-        return response
+        subjects = get_subjects(self.path)
+        return Response(subjects)
 
 
 class SearchNews(APIView):
+    path = '/noticias/_search'
 
     def get(self, request, format=None):
         words = request.query_params.get('search', None)
         if words:
-            news = self.get_filter_news(words)
+            subjects = get_filter_subjects(self.path, words)
         else:
-            news = {'error': _('It is necessary to pass search')}
-        return Response(news)
+            subjects = {'error': _('It is necessary to pass search')}
+        return Response(subjects)
 
-    def get_filter_news(self, words):
-        path = '/noticias/_search'
 
-        query = DEFAULT_QUERY.replace('replace_query', SEARCH_QUERY)
-        query = query.replace('words', words)
+class ListRadioagency(APIView):
+    path = '/radioagencia/_search'
 
-        response = requests.request('POST', settings.API_DITEC + path,
-                                    headers=HEADERS,
-                                    data=query)
-        try:
-            response = response.json()
-        except Exception:
-            response = {'error': _('Error not found news')}
+    def get(self, request):
+        subjects = get_subjects(self.path)
+        return Response(subjects)
 
-        return response
+
+class SearchRadioagency(APIView):
+    path = '/radioagencia/_search'
+
+    def get(self, request, format=None):
+        words = request.query_params.get('search', None)
+        if words:
+            subjects = get_filter_subjects(self.path, words)
+        else:
+            subjects = {'error': _('It is necessary to pass search')}
+        return Response(subjects)
+
+
+def get_subjects(path):
+    query = DEFAULT_QUERY.replace('replace_query', DATE_QUERY)
+    query = query.replace('NW', NUMBER_WEEKS)
+
+    response = requests.request('POST', settings.API_DITEC + path,
+                                headers=HEADERS,
+                                data=query)
+    try:
+        response = response.json()
+    except Exception:
+        response = {'error': _('Error not found news')}
+
+    return response
+
+
+def get_filter_subjects(path, words):
+
+    query = DEFAULT_QUERY.replace('replace_query', SEARCH_QUERY)
+    query = query.replace('words', words)
+
+    response = requests.request('POST', settings.API_DITEC + path,
+                                headers=HEADERS,
+                                data=query)
+    try:
+        response = response.json()
+    except Exception:
+        response = {'error': _('Error not found news')}
+
+    return response
