12f596b9b02de23bb777ce4cfd2b15760016e2eb
erivanio <erivanio.vanin@gmail.com>
2020-08-10 18:12:05 -0300

    create confirm password and add translations

--- src/linguagemsimples/urls.py
+++ src/linguagemsimples/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -27,10 +27,17 @@ urlpatterns = [
          name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
          name='schema-redoc'),
-    path('admin/', admin.site.urls),
     path('api/', include('apps.authentication.urls')),
     path('api/', include('apps.plenary_session.urls')),
     path('api/', include('apps.api_ditec.urls')),
-    path('teste/', TemplateView.as_view(template_name="components/base.html")),
-    path('', include('django.contrib.auth.urls')),
+    path('password_reset/', auth_views.PasswordResetView.as_view(),
+         name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
+    path('admin/', admin.site.urls),
 ]
